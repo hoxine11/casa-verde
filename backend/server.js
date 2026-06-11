70545039818977530d686fc752d8413c685133ab
hoxine11 <hocinebaslimane2003@gmail.com>
2026-06-11 13:15:41 +0100

    Backend ready for Railway

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,8 @@ app.use(
   adminRoutes
 );
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Server running on port ${process.env.PORT}`
-  );
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
