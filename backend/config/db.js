dd64d4c150821aaed3f07fa973cf946dde8e188b
hoxine11 <hocinebaslimane2003@gmail.com>
2026-06-12 10:13:10 +0100

    Use Railway PostgreSQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import pkg from "pg";
 
 const { Pool } = pkg;
 
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD ? "OK" : "MISSING"
-});
-
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
