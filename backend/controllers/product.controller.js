761621ed57f1418b534eff82d698c883f82e2f7b
hoxine11 <hocinebaslimane2003@gmail.com>
2026-06-16 11:10:33 +0100

    add variants and options

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -11,12 +11,26 @@ export const createProduct = async (req, res) => {
       is_active
     } = req.body;
 
-    const resultImage =
-      await cloudinary.uploader.upload(req.file.path);
+    const variants = JSON.parse(
+      req.body.variants || "[]"
+    );
 
-    const imageUrl = resultImage.secure_url;
+    const options = JSON.parse(
+      req.body.options || "[]"
+    );
 
-    const result = await pool.query(
+    let imageUrl = null;
+
+    if (req.file) {
+      const resultImage =
+        await cloudinary.uploader.upload(
+          req.file.path
+        );
+
+      imageUrl = resultImage.secure_url;
+    }
+
+    const productResult = await pool.query(
       `
       INSERT INTO products
       (
@@ -37,16 +51,65 @@ export const createProduct = async (req, res) => {
         imageUrl,
         price,
         category_id,
-        is_active
+        is_active === "true",
       ]
     );
 
-    res.status(201).json(result.rows[0]);
+    const product = productResult.rows[0];
+
+    // Sauvegarde des tailles
+    for (const variant of variants) {
+      if (!variant.name) continue;
+
+      await pool.query(
+        `
+        INSERT INTO product_variants
+        (
+          product_id,
+          name,
+          price
+        )
+        VALUES ($1,$2,$3)
+        `,
+        [
+          product.id,
+          variant.name,
+          variant.price || 0
+        ]
+      );
+    }
+
+    // Sauvegarde des options
+    for (const option of options) {
+      if (!option.name) continue;
+
+      await pool.query(
+        `
+        INSERT INTO product_options
+        (
+          product_id,
+          name,
+          price
+        )
+        VALUES ($1,$2,$3)
+        `,
+        [
+          product.id,
+          option.name,
+          option.price || 0
+        ]
+      );
+    }
+
+    return res.status(201).json({
+      message: "Produit créé avec succès",
+      product
+    });
 
   } catch (error) {
     console.log(error);
 
-    res.status(500).json({
+    return res.status(500).json({
       error: error.message
     });
   }
